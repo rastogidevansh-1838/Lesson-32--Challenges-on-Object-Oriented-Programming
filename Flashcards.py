cd5a347782fea40d915b6254c807463c86236a91
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-05-10 12:19:12 +0530

    flashcard create one

---
+++ Flashcards.py
@@ -0,0 +1,18 @@
+class Flashcard:
+    def __init__ (self, word, meaning):
+        self.word = word
+        self.meaning = meaning
+    def __str__(self):
+        return self.word+'('+self.meaning+')'
+flash = []
+print("Welcome to flashcard application")
+while(True):
+    word = input("Enter the name you want to add to the flashcard: ")
+    meaning = input("Enter the meaning of the word: ")
+    flash.append(Flashcard(word, meaning))
+    option = int(input("Enter 0. If you want to add another flashcard enter 1:"))
+    if(option):
+        break
+print("\nYour Flashcards")
+for i in flash:
+    print(">", flash)
